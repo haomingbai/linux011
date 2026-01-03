9ccce6688bd5b96629aa61e10f55db4eb289dd23
Haoming Bai <haomingbai@hotmail.com>
2026-01-03 22:37:13 +0800

    Format the code with Google style.

--- include/stddef.h
+++ include/stddef.h
@@ -12,8 +12,8 @@ typedef unsigned long size_t;
 #endif
 
 #undef NULL
-#define NULL ((void *)0)
+#define NULL ((void*)0)
 
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#define offsetof(TYPE, MEMBER) ((size_t)&((TYPE*)0)->MEMBER)
 
 #endif
--- include/string.h
+++ include/string.h
@@ -2,7 +2,7 @@
 #define _STRING_H_
 
 #ifndef NULL
-#define NULL ((void *) 0)
+#define NULL ((void*)0)
 #endif
 
 #ifndef _SIZE_T
@@ -10,7 +10,7 @@
 typedef unsigned int size_t;
 #endif
 
-extern char * strerror(int errno);
+extern char* strerror(int errno);
 
 /*
  * This string-include defines all string functions as inline
@@ -23,25 +23,25 @@ extern char * strerror(int errno);
  *
  *		(C) 1991 Linus Torvalds
  */
- 
-extern inline char * strcpy(char * dest,const char *src);
-extern inline char * strcat(char * dest,const char * src);
-extern inline int strcmp(const char * cs,const char * ct);
-extern inline int strspn(const char * cs, const char * ct);
-extern inline int strcspn(const char * cs, const char * ct);
-extern inline char * strpbrk(const char * cs,const char * ct);
-extern inline char * strstr(const char * cs,const char * ct);
-extern inline int strlen(const char * s);
-extern char * ___strtok;
 
-extern inline char * strtok(char * s,const char * ct);
+extern inline char* strcpy(char* dest, const char* src);
+extern inline char* strcat(char* dest, const char* src);
+extern inline int strcmp(const char* cs, const char* ct);
+extern inline int strspn(const char* cs, const char* ct);
+extern inline int strcspn(const char* cs, const char* ct);
+extern inline char* strpbrk(const char* cs, const char* ct);
+extern inline char* strstr(const char* cs, const char* ct);
+extern inline int strlen(const char* s);
+extern char* ___strtok;
+
+extern inline char* strtok(char* s, const char* ct);
 
 /*
  * Changes by falcon<zhangjinw@gmail.com>, the original return value is static
  * inline ... it can not be called by other functions in another files.
  */
 
-extern inline void * memcpy(void * dest,const void * src, int n);
-extern inline void * memmove(void * dest,const void * src, int n);
-extern inline void * memchr(const void * cs,char c,int count);
+extern inline void* memcpy(void* dest, const void* src, int n);
+extern inline void* memmove(void* dest, const void* src, int n);
+extern inline void* memchr(const void* cs, char c, int count);
 #endif
